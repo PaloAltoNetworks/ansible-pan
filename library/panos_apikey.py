8860d4043a10c956e77199631837dd4b484d2da2
John Anderson <lampwins@gmail.com>
2016-05-20 12:12:31 -0400

    ability to generate a new api key

---
+++ library/panos_apikey.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2014, Palo Alto Networks <techbizdev@paloaltonetworks.com>
+#
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+DOCUMENTATION = '''
+---
+module: panos_apikey
+short_description: generate an api key
+description:
+    - Use the XML API to generate a new API Key for later use
+author:
+    - John Anderson
+version_added: "0.0"
+requirements:
+    - pan-python
+options:
+    ip_address:
+        description:
+            - IP address (or hostname) of PAN-OS device
+        required: true
+    password:
+        description:
+            - password for authentication
+        required: true
+    username:
+        description:
+            - username for authentication
+        required: false
+        default: "admin"
+    timeout:
+        description:
+            - timeout of API calls
+        required: false
+        default: "0"
+'''
+
+EXAMPLES = '''
+# 192.168.1.1 with credentials admin/admin
+- name: generate key
+  panos_apikey:
+    ip_address: "192.168.1.1"
+    password: "admin"
+'''
+
+import sys
+
+try:
+    import pan.xapi
+except ImportError:
+    print "failed=True msg='pan-python required for this module'"
+    sys.exit(1)
+
+
+def main():
+    argument_spec = dict(
+        ip_address=dict(default=None),
+        password=dict(default=None, no_log=True),
+        username=dict(default='admin'),
+        timeout=dict(default=0, type='int')
+    )
+    module = AnsibleModule(argument_spec=argument_spec)
+
+    ip_address = module.params["ip_address"]
+    if not ip_address:
+        module.fail_json(msg="ip_address should be specified")
+    password = module.params["password"]
+    if not password:
+        module.fail_json(msg="password is required")
+    username = module.params['username']
+    timeout = module.params['timeout']
+
+    xapi = pan.xapi.PanXapi(
+        hostname=ip_address,
+        api_username=username,
+        api_password=password,
+        timeout=timeout
+    )
+
+    try:
+        key = xapi.keygen()
+        module.exit_json(changed=True, msg=key)
+    except:
+        pass
+
+    module.fail_json(msg="Failed")
+
+from ansible.module_utils.basic import *   # noqa
+
+main()
