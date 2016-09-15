8b19668166ec08922202bf3ff9fd235ebd03ed5f
Ivan Bojer <ijb@networkrift.com>
2016-09-15 13:49:20 -0700

    file rename

--- tools/rsync_to_ansible.sh
+++ tools/rsync_to_ansible.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 #rsync ../library/panos_admpwd ../../ansible-modules-extras/network/panos/
-rsync ../library/panos_admin ../../ansible-modules-extras/network/panos/
+rsync ../library/__init__.py ../../ansible-modules-extras/network/panos/
+rsync ../library/panos_admin.py ../../ansible-modules-extras/network/panos/
