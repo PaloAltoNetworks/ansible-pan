1c70370e049c1c9ea63e59dc19e1bd7d04076f6a
Ivan Bojer <ijb@networkrift.com>
2016-10-03 09:05:49 -0700

    changed version to 2.2 as 2.3 was failing automated tests

--- library/panos_admin.py
+++ library/panos_admin.py
@@ -27,7 +27,7 @@ description:
     - PanOS module that allows changes to the user account passwords by doing
       API calls to the Firewall using pan-api as the protocol.
 author: "Luigi Mori (@jtschichold), Ivan Bojer (@ivanbojer)"
-version_added: "2.3"
+version_added: "2.2"
 requirements:
     - pan-python
 options:
