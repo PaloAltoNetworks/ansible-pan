95656cac926625c9266ec36fba2581f576578166
Ivan Bojer <ijb@networkrift.com>
2016-10-18 14:21:08 -0700

    add admpwd to inclusion list

--- tools/rsync_to_ansible.sh
+++ tools/rsync_to_ansible.sh
@@ -5,4 +5,4 @@
 
 rsync ../library/__init__.py ../../ansible-modules-extras/network/panos/
 rsync ../library/panos_admin.py ../../ansible-modules-extras/network/panos/
-#rsync ../library/panos_admpwd ../../ansible-modules-extras/network/panos/
+rsync ../library/panos_admpwd.py ../../ansible-modules-extras/network/panos/
