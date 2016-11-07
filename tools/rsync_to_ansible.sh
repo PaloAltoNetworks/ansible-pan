cb7449f69b3591b335cfc7ba9048a39196efd564
Ivan Bojer <ijb@networkrift.com>
2016-11-07 13:56:52 -0800

    new module submission to ANSIBLE

--- tools/rsync_to_ansible.sh
+++ tools/rsync_to_ansible.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 ####
-# This script copies modules that are changed
+# This script copies modules that are changed. Run it with caution as it
+# will overwrite destination files
 
 rsync ../library/__init__.py ../../ansible-modules-extras/network/panos/
 rsync ../library/panos_admin.py ../../ansible-modules-extras/network/panos/
 rsync ../library/panos_admpwd.py ../../ansible-modules-extras/network/panos/
 rsync ../library/panos_commit.py ../../ansible-modules-extras/network/panos/
+rsync ../library/panos_restart.py ../../ansible-modules-extras/network/panos/
