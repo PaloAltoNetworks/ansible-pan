84842bd5d9283c1f708e062e1526f221889aecbb
Ivan Bojer <ijb@networkrift.com>
2017-01-03 14:47:10 -0800

    - updated list of extra converted modules

--- tools/rsync_to_ansible.sh
+++ tools/rsync_to_ansible.sh
@@ -4,8 +4,29 @@
 # This script copies modules that are changed. Run it with caution as it
 # will overwrite destination files
 
+# batch 1
 rsync ../library/__init__.py ../../ansible-modules-extras/network/panos/
 rsync ../library/panos_admin.py ../../ansible-modules-extras/network/panos/
 rsync ../library/panos_admpwd.py ../../ansible-modules-extras/network/panos/
 rsync ../library/panos_commit.py ../../ansible-modules-extras/network/panos/
 rsync ../library/panos_restart.py ../../ansible-modules-extras/network/panos/
+
+# batch 2
+rsync ../library/panos_cert_gen_ssh.py ../../ansible-modules-extras/network/panos/
+rsync ../library/panos_check.py ../../ansible-modules-extras/network/panos/
+rsync ../library/panos_dag.py ../../ansible-modules-extras/network/panos/
+rsync ../library/panos_lic.py ../../ansible-modules-extras/network/panos/
+rsync ../library/panos_nat.py ../../ansible-modules-extras/network/panos/
+rsync ../library/panos_mgtconfig.py ../../ansible-modules-extras/network/panos/
+rsync ../library/panos_swapif.py ../../ansible-modules-extras/network/panos/
+rsync ../library/panos_swinstall.py ../../ansible-modules-extras/network/panos/
+rsync ../library/panos_loadcfg.py ../../ansible-modules-extras/network/panos/
+rsync ../library/panos_content.py ../../ansible-modules-extras/network/panos/
+rsync ../library/panos_import.py ../../ansible-modules-extras/network/panos/
+rsync ../library/panos_dhcpif.py ../../ansible-modules-extras/network/panos/
+rsync ../library/panos_pg.py ../../ansible-modules-extras/network/panos/
+rsync ../library/panos_service.py ../../ansible-modules-extras/network/panos/
+rsync ../library/panos_tunnelif.py ../../ansible-modules-extras/network/panos/
+rsync ../library/panos_vulnprofile.py ../../ansible-modules-extras/network/panos/
+rsync ../library/panos_cstapphost.py ../../ansible-modules-extras/network/panos/
+rsync ../library/panos_gpp_gateway.py ../../ansible-modules-extras/network/panos/
