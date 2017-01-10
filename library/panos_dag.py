181520881fde708714ea5b41414fe660e0def13d
Ivan Bojer <ijb@networkrift.com>
2017-01-10 14:53:04 -0800

    - Changes based on the ANSIBLE module development requirements

--- library/panos_dag.py
+++ library/panos_dag.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 
-# Copyright (c) 2014, Palo Alto Networks <techbizdev@paloaltonetworks.com>
+# Copyright (c) 2016, Palo Alto Networks <techbizdev@paloaltonetworks.com>
 #
 # Permission to use, copy, modify, and/or distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
@@ -19,11 +19,9 @@ DOCUMENTATION = '''
 module: panos_dag
 short_description: create a dynamic address group
 description:
-    - Create a dynamic address group
-author: 
-    - Palo Alto Networks 
-    - Luigi Mori (jtschichold)
-version_added: "0.0"
+    - Create a dynamic address group object in the firewall used for policy rules
+author: "Luigi Mori (@jtschichold), Ivan Bojer (@ivanbojer)"
+version_added: "2.3"
 requirements:
     - pan-python
 options:
@@ -68,13 +66,21 @@ EXAMPLES = '''
     dag_filter: "'aws-tag.aws:cloudformation:logical-id.ServerInstance' and 'instanceState.running'"
 '''
 
-import sys
+RETURN='''
+# Default return values
+'''
+
+ANSIBLE_METADATA = {'status': ['preview'],
+                    'supported_by': 'community',
+                    'version': '1.0'}
+
+from ansible.module_utils.basic import AnsibleModule
 
 try:
     import pan.xapi
+    HAS_LIB = True
 except ImportError:
-    print "failed=True msg='pan-python required for this module'"
-    sys.exit(1)
+    HAS_LIB = False
 
 _ADDRGROUP_XPATH = "/config/devices/entry[@name='localhost.localdomain']" +\
                    "/vsys/entry[@name='vsys1']/address-group/entry[@name='%s']"
@@ -107,21 +113,19 @@ def add_dag(xapi, dag_name, dag_filter):
 
 def main():
     argument_spec = dict(
-        ip_address=dict(default=None),
-        password=dict(default=None),
+        ip_address=dict(required=True),
+        password=dict(required=True),
         username=dict(default='admin'),
-        dag_name=dict(default=None),
-        dag_filter=dict(default=None),
+        dag_name=dict(required=True),
+        dag_filter=dict(required=True),
         commit=dict(type='bool', default=True)
     )
-    module = AnsibleModule(argument_spec=argument_spec)
+    module = AnsibleModule(argument_spec=argument_spec, supports_check_mode=False)
+    if not HAS_LIB:
+        module.fail_json(msg='pan-python is required for this module')
 
     ip_address = module.params["ip_address"]
-    if not ip_address:
-        module.fail_json(msg="ip_address should be specified")
     password = module.params["password"]
-    if not password:
-        module.fail_json(msg="password is required")
     username = module.params['username']
 
     xapi = pan.xapi.PanXapi(
@@ -131,11 +135,7 @@ def main():
     )
 
     dag_name = module.params['dag_name']
-    if not dag_name:
-        module.fail_json(msg="dag_name is required")
     dag_filter = module.params['dag_filter']
-    if not dag_filter:
-        module.fail_json(msg="dag_filter is required")
     commit = module.params['commit']
 
     changed = add_dag(xapi, dag_name, dag_filter)
@@ -145,6 +145,5 @@ def main():
 
     module.exit_json(changed=changed, msg="okey dokey")
 
-from ansible.module_utils.basic import *  # noqa
-
-main()
+if __name__ == '__main__':
+    main()
