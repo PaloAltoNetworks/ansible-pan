488e79e6458e8c23cbf5e3ef70de7f114bd93a56
Ivan Bojer <ijb@networkrift.com>
2017-01-10 14:53:55 -0800

    - Changes based on the ANSIBLE module development requirements

--- library/panos_cert_gen_ssh.py
+++ library/panos_cert_gen_ssh.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 
-# Copyright (c) 2014, Palo Alto Networks <techbizdev@paloaltonetworks.com>
+# Copyright (c) 2016, Palo Alto Networks <techbizdev@paloaltonetworks.com>
 #
 # Permission to use, copy, modify, and/or distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
@@ -20,8 +20,8 @@ module: panos_cert_gen_ssh
 short_description: generates a self-signed certificate - NOT A CA -- using SSH with SSH key
 description:
     - generate certificate
-author: Palo Alto Networks
-version_added: "0.1"
+author: "Luigi Mori (@jtschichold), Ivan Bojer (@ivanbojer)"
+version_added: "2.3"
 requirements:
     - paramiko
 options:
@@ -73,15 +73,26 @@ EXAMPLES = '''
     signed_by: "root-ca"
 '''
 
-import sys
+RETURN='''
+# Default return values
+'''
+
+ANSIBLE_METADATA = {'status': ['preview'],
+                    'supported_by': 'community',
+                    'version': '1.0'}
+
+from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.basic import get_exception
+import time
 
 try:
     import paramiko
+    HAS_LIB=True
 except ImportError:
-    print "failed=True msg='paramiko required for this module'"
-    sys.exit(1)
+    HAS_LIB=False
 
 _PROMPTBUFF = 4096
+
 
 def wait_with_timeout(module, shell, prompt, timeout=60):
     now = time.time()
@@ -97,6 +108,7 @@ def wait_with_timeout(module, shell, prompt, timeout=60):
             module.fail_json(msg="Timeout waiting for prompt")
 
     return result
+
 
 def generate_cert(module, ip_address, key_filename, password,
                   cert_cn, cert_friendly_name, signed_by, rsa_nbits ):
@@ -140,54 +152,41 @@ def generate_cert(module, ip_address, key_filename, password,
 
 def main():
     argument_spec = dict(
-        ip_address=dict(default=None),
-        key_filename=dict(default=None),
-        password=dict(default=None),
-        cert_cn=dict(default=None),
-        cert_friendly_name=dict(default=None),
+        ip_address=dict(required=True),
+        key_filename=dict(),
+        password=dict(),
+        cert_cn=dict(required=True),
+        cert_friendly_name=dict(required=True),
         rsa_nbits=dict(default='1024'),
-        signed_by=dict(default=None)
+        signed_by=dict(required=True)
 
     )
-    module = AnsibleModule(argument_spec=argument_spec)
+    module = AnsibleModule(argument_spec=argument_spec, supports_check_mode=False, required_one_of=[['key_filename', 'password']])
+    if not HAS_LIB:
+        module.fail_json(msg='paramiko is required for this module')
 
     ip_address = module.params["ip_address"]
-    if not ip_address:
-        module.fail_json(msg="ip_address should be specified")
-
     key_filename = module.params["key_filename"]
-    password = None
-    if not key_filename:
-        password = module.params["password"]
-        if not password:
-            module.fail_json(msg="either key or password is required")
-
+    password = module.params["password"]
     cert_cn = module.params["cert_cn"]
-    if not cert_cn:
-        module.fail_json(msg="cert_cn is required")
-
     cert_friendly_name = module.params["cert_friendly_name"]
-    if not cert_friendly_name:
-        module.fail_json(msg="cert_friendly_name is required")
-
     signed_by = module.params["signed_by"]
-    if not signed_by:
-        module.fail_json(msg="signed_by is required")
-
     rsa_nbits = module.params["rsa_nbits"]
 
-    stdout = generate_cert(module,
-                           ip_address,
-                           key_filename,
-                           password,
-                           cert_cn,
-                           cert_friendly_name,
-                           signed_by,
-                           rsa_nbits)
+    try:
+        stdout = generate_cert(module,
+                               ip_address,
+                               key_filename,
+                               password,
+                               cert_cn,
+                               cert_friendly_name,
+                               signed_by,
+                               rsa_nbits)
+    except Exception:
+        exc = get_exception()
+        module.fail_json(msg=exc.message)
 
     module.exit_json(changed=True, msg="okey dokey")
 
-
-from ansible.module_utils.basic import *  # noqa
-
-main()
+if __name__ == '__main__':
+    main()
