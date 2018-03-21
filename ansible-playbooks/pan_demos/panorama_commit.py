f58a78365eda4bc7ab7abe5175260e7fb78feb4e
vinayvenkat <vinayvenkat@yahoo.com>
2018-03-21 12:08:06 -0700

    Fixed a minor python artifact

--- ansible-playbooks/pan_demos/panorama_commit.py
+++ ansible-playbooks/pan_demos/panorama_commit.py
@@ -6,8 +6,8 @@ from pandevice import panorama
 def commit_panorma():
     "Commit configuration on Panorma to the FW's"
 
-    panorama = panorama.Panorama('10.8.202.117', 'admin', 'admin')
-    panorama.commit_all(sync=True, sync_all=True, exception=True, devicegroup='cap_one_dg')
+    _panorama = panorama.Panorama('10.8.202.117', 'admin', 'admin')
+    _panorama.commit_all(sync=True, sync_all=True, exception=True, devicegroup='cap_one_dg')
 
 if __name__ == "__main__":
     commit_panorma()
