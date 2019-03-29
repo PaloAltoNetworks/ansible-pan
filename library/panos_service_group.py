2e96362df40774977ac39abd54365e7df582a4ea
Michael Richardson <mrichardson03@gmail.com>
2019-03-29 16:10:02 -0400

    Updating panos_service_group

--- library/panos_service_group.py
+++ library/panos_service_group.py
@@ -34,22 +34,13 @@ requirements:
     - pandevice can be obtained from PyPI U(https://pypi.python.org/pypi/pandevice)
 notes:
     - Panorama is supported.
-    - Check mode is not supported.
+    - Check mode is supported.
+extends_documentation_fragment:
+    - panos.transitional_provider
+    - panos.vsys
+    - panos.device_group
+    - panos.state
 options:
-    ip_address:
-        description:
-            - IP address or hostname of PAN-OS device.
-        required: true
-    username:
-        description:
-            - Username for authentication for PAN-OS device.  Optional if I(api_key) is used.
-        default: 'admin'
-    password:
-        description:
-            - Password for authentication for PAN-OS device.  Optional if I(api_key) is used.
-    api_key:
-        description:
-            - API key to be used instead of I(username) and I(password).
     name:
         description:
             - Name of service group.
@@ -64,24 +55,12 @@ options:
         description:
             - List of tags for this service group.
         type: list
-    device_group:
-        description:
-            - If I(ip_address) is a Panorama device, create object in this device group.
-    vsys:
-        description:
-            - If I(ip_address) is a firewall, create object in this virtual system.
-        type: string
-        default: 'vsys1'
-    state:
-        description:
-            - Create or remove service group object.
-        choices: ['present', 'absent']
-        default: 'present'
     commit:
         description:
             - Commit changes after creating object.  If I(ip_address) is a Panorama device, and I(device_group) is
               also set, perform a commit to Panorama and a commit-all to the device group.
         required: false
+        type: bool
         default: true
 '''
 
@@ -108,163 +87,65 @@ RETURN = '''
 '''
 
 from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.network.panos.panos import get_connection
 
 try:
-    from pandevice import base
-    from pandevice import firewall
-    from pandevice import objects
-    from pandevice import panorama
+    from pandevice.objects import ServiceGroup
     from pandevice.errors import PanDeviceError
-
-    HAS_LIB = True
 except ImportError:
-    HAS_LIB = False
-
-
-def add_object(device, obj, device_group=None):
-    if isinstance(device, firewall.Firewall):
-        return device.add(obj)
-    elif isinstance(device, panorama.Panorama):
-        if device_group:
-            return get_devicegroup(device, device_group).add(obj)
-        else:
-            return device.add(obj)
-
-    return None
-
-
-def find_object(device, obj_name, obj_type, device_group=None):
-    obj_type.refreshall(device)
-
-    if isinstance(device, firewall.Firewall):
-        return device.find(obj_name, obj_type)
-    elif isinstance(device, panorama.Panorama):
-        if device_group:
-            dg = get_devicegroup(device, device_group)
-            device.add(dg)
-            obj_type.refreshall(dg)
-            return dg.find(obj_name, obj_type)
-        else:
-            return device.find(obj_name, obj_type)
-
-    return None
-
-
-def get_devicegroup(device, device_group):
-
-    if isinstance(device, panorama.Panorama):
-        dgs = device.refresh_devices()
-
-        for dg in dgs:
-            if isinstance(dg, panorama.DeviceGroup):
-                if dg.name == device_group:
-                    return dg
-
-    return None
-
-
-def perform_commit(module, device, device_group):
-    if isinstance(device, firewall.Firewall):
-        result = device.commit(sync=True)
-
-        if result:
-            check_commit_result(module, result)
-
-    elif isinstance(device, panorama.Panorama):
-        result = device.commit(sync=True)
-
-        if result:
-            check_commit_result(module, result)
-
-        if device_group:
-            result = device.commit_all(sync=True, sync_all=True, devicegroup=device_group)
-
-            if result:
-                check_commit_result(module, result)
-
-
-def check_commit_result(module, result):
-    if result['result'] == 'FAIL':
-        module.fail_json(msg='Commit failed')
+    pass
 
 
 def main():
-    argument_spec = dict(
-        ip_address=dict(required=True),
-        username=dict(default='admin'),
-        password=dict(no_log=True),
-        api_key=dict(no_log=True),
-        name=dict(type='str', required=True),
-        value=dict(type='list'),
-        tag=dict(type='list'),
-        device_group=dict(type='str'),
-        vsys=dict(type='str', default='vsys1'),
-        state=dict(default='present', choices=['present', 'absent']),
-        commit=dict(type='bool', default=True)
+    helper = get_connection(
+        vsys=True,
+        device_group=True,
+        with_classic_provider_spec=True,
+        with_state=True,
+        argument_spec=dict(
+            name=dict(type='str', required=True),
+            value=dict(type='list'),
+            tag=dict(type='list'),
+            commit=dict(type='bool', default=True)
+        )
     )
 
-    module = AnsibleModule(argument_spec=argument_spec, supports_check_mode=False)
+    module = AnsibleModule(
+        argument_spec=helper.argument_spec,
+        supports_check_mode=True
+    )
 
-    if not HAS_LIB:
-        module.fail_json(msg='pan-python and pandevice are required for this module.')
+    # Verify libs are present, get parent object.
+    parent = helper.get_pandevice_parent(module)
 
-    ip_address = module.params['ip_address']
-    username = module.params['username']
-    password = module.params['password']
-    api_key = module.params['api_key']
-    name = module.params['name']
-    value = module.params['value']
-    tag = module.params['tag']
-    device_group = module.params['device_group']
-    vsys = module.params['vsys']
-    state = module.params['state']
+    # Object params.
+    spec = {
+        'name': module.params['name'],
+        'value': module.params['value'],
+        'tag': module.params['tag']
+    }
+
+    # Other info.
     commit = module.params['commit']
 
-    changed = False
-
+    # Retrieve current info.
     try:
-        device = base.PanDevice.create_from_device(ip_address, username, password, api_key=api_key)
-
-        if isinstance(device, firewall.Firewall):
-            device.vsys = vsys
-
-        if device_group:
-            if device_group.lower() == 'shared':
-                device_group = None
-            else:
-                if not get_devicegroup(device, device_group):
-                    module.fail_json(msg='Could not find {} device group.'.format(device_group))
-
-        if state == 'present':
-            if not value:
-                module.fail_json(msg='Must specify \'value\' if \'state\' is \'present\'.')
-
-            existing_obj = find_object(device, name, objects.ServiceGroup, device_group)
-            new_obj = objects.ServiceGroup(name=name, value=value, tag=tag)
-
-            if not existing_obj:
-                add_object(device, new_obj, device_group)
-                new_obj.create()
-                changed = True
-            elif not existing_obj.equal(new_obj):
-                existing_obj.value = value
-                existing_obj.tag = tag
-                existing_obj.apply()
-                changed = True
-
-        elif state == 'absent':
-            existing_obj = find_object(device, name, objects.ServiceGroup, device_group)
-
-            if existing_obj:
-                existing_obj.delete()
-                changed = True
-
-        if commit and changed:
-            perform_commit(module, device, device_group)
-
+        listing = ServiceGroup.refreshall(parent, add=False)
     except PanDeviceError as e:
-        module.fail_json(msg=e.message)
+        module.fail_json('Failed refresh: {0}'.format(e))
 
+    # Build the object based on the user spec.
+    obj = ServiceGroup(**spec)
+    parent.add(obj)
+
+    # Apply the state.
+    changed = helper.apply_state(obj, listing, module)
+
+    # Commit.
+    if commit and changed:
+        helper.commit(module)
+
+    # Done.
     module.exit_json(changed=changed)
 
 
