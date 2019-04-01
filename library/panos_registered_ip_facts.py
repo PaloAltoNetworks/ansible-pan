4a549580ffd96385fd22d0466871e00c259006d0
Michael Richardson <mrichardson03@gmail.com>
2019-04-01 16:43:12 -0400

    Updated panos_registered_ip_facts

--- library/panos_registered_ip_facts.py
+++ library/panos_registered_ip_facts.py
@@ -34,21 +34,9 @@ requirements:
     - pandevice can be obtained from PyPI U(https://pypi.python.org/pypi/pandevice)
 notes:
     - Panorama is not supported.
+extends_documentation_fragment:
+    - panos.transitional_provider
 options:
-    ip_address:
-        description:
-            - IP address or hostname of PAN-OS device.
-        required: true
-    username:
-        description:
-            - Username for authentication for PAN-OS device.  Optional if I(api_key) is used.
-        default: 'admin'
-    password:
-        description:
-            - Password for authentication for PAN-OS device.  Optional if I(api_key) is used.
-    api_key:
-        description:
-            - API key to be used instead of I(username) and I(password).
     tags:
         description:
             - List of tags to retrieve facts for.  If not specified, retrieve all tags.
@@ -57,16 +45,12 @@ options:
 EXAMPLES = '''
 - name: Get facts for all registered IPs
   panos_registered_ip_facts:
-    ip_address: '{{ fw_ip_address }}'
-    username: '{{ fw_username }}'
-    password: '{{ fw_password }}'
+    provider: '{{ provider }}'
   register: registered_ip_facts
 
 - name: Get facts for specific tag
   panos_registered_ip_facts:
-    ip_address: '{{ fw_ip_address }}'
-    username: '{{ fw_username }}'
-    password: '{{ fw_password }}'
+    provider: '{{ provider }}'
     tags: ['First_Tag']
   register: first_tag_registered_ip_facts
 '''
@@ -79,42 +63,38 @@ results:
     sample: { '1.1.1.1': ['First_Tag', 'Second_Tag'] }
 '''
 
-from ansible.module_utils.basic import AnsibleModule, get_exception
+from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.network.panos.panos import get_connection
 
 try:
-    from pandevice import base
     from pandevice.errors import PanDeviceError
-
-    HAS_LIB = True
 except ImportError:
-    HAS_LIB = False
+    pass
 
 
 def main():
-    argument_spec = dict(
-        ip_address=dict(required=True),
-        username=dict(default='admin'),
-        password=dict(no_log=True),
-        api_key=dict(no_log=True),
-        tags=dict(type='list')
+    helper = get_connection(
+        # vsys=True,
+        with_classic_provider_spec=True,
+        argument_spec=dict(
+            tags=dict(type='list')
+        )
     )
 
-    module = AnsibleModule(argument_spec=argument_spec, supports_check_mode=False)
-    if not HAS_LIB:
-        module.fail_json(msg='pan-python and pandevice are required for this module.')
+    module = AnsibleModule(
+        argument_spec=helper.argument_spec,
+        required_one_of=helper.required_one_of
+    )
 
-    ip_address = module.params['ip_address']
-    username = module.params['username']
-    password = module.params['password']
-    api_key = module.params['api_key']
     tags = module.params['tags']
 
+    device = helper.get_pandevice_parent(module)
+
     try:
-        device = base.PanDevice.create_from_device(ip_address, username, password, api_key=api_key)
         registered_ips = device.userid.get_registered_ip(tags=tags)
 
-    except PanDeviceError:
-        module.fail_json(msg=get_exception())
+    except PanDeviceError as e:
+        module.fail_json(msg='Failed get_registered_ip: {0}'.format(e))
 
     module.exit_json(changed=False, results=registered_ips)
 
