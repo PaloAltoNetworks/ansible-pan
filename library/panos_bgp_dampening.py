1d2a8a0e38a6137cb77c379e5475d1a69b6f0b8e
Garfield Lee Freeman <gfreeman@paloaltonetworks.com>
2019-04-04 12:53:37 -0700

    Updating panos_bgp_dampening (#309)

--- library/panos_bgp_dampening.py
+++ library/panos_bgp_dampening.py
@@ -29,77 +29,65 @@ short_description: Configures a BGP Dampening Profile
 description:
     - Use BGP to publish and consume routes from disparate networks.
 author: "Joshua Colson (@freakinhippie)"
-version_added: "2.9"
+version_added: "2.8"
 requirements:
     - pan-python can be obtained from PyPI U(https://pypi.python.org/pypi/pan-python)
     - pandevice can be obtained from PyPI U(https://pypi.python.org/pypi/pandevice)
 notes:
-    - Checkmode is not supported.
-    - Panorama is NOT supported.
+    - Checkmode is supported.
+    - Panorama is supported.
+extends_documentation_fragment:
+    - panos.transitional_provider
+    - panos.full_template_support
+    - panos.state
 options:
-    ip_address:
-        description:
-            - IP address (or hostname) of PAN-OS device being configured.
-            required: True
-    username:
-        description:
-            - Username credentials to use for auth unless I(api_key) is set.
-            default: admin
-    password:
-        description:
-            - Password credentials to use for auth unless I(api_key) is set.
-    api_key:
-        description:
-            - API key that can be used instead of I(username)/I(password) credentials.
-    state:
-        description:
-            - Add or remove BGP Dampening Profile.
-                - present
-                - absent
-            default: present
     commit:
         description:
             - Commit configuration if changed.
-            default: True
+        default: True
+        type: bool
     vr_name:
         description:
-            - Name of the virtual router; it must already exist; see panos_virtual_router.
-            default: default
+            - Name of the virtual router; it must already exist.
+            - See M(panos_virtual_router).
+        default: 'default'
     cutoff:
         description:
             - Cutoff threshold value.
+        type: float
     decay_half_life_reachable:
         description:
             - Decay half-life while reachable (in seconds).
+        type: int
     decay_half_life_unreachable:
         description:
             - Decay half-life while unreachable (in seconds).
+        type: int
     enable:
         description:
             - Enable profile.
-            default: True
+        default: True
+        type: bool
     max_hold_time:
         description:
             - Maximum of hold-down time (in seconds).
+        type: int
     name:
         description:
             - Name of Dampening Profile.
-            required: True
+        required: True
     reuse:
         description:
             - Reuse threshold value.
+        type: float
 '''
 
 EXAMPLES = '''
 - name: Create BGP Dampening Profile
-    panos_bgp_dampening:
-      ip_address: '{{ ip_address }}'
-      username: '{{ username }}'
-      password: '{{ password }}'
-      state: 'present'
-      name: damp-profile-1
-      enable: true
-      commit: true
+  panos_bgp_dampening:
+    name: damp-profile-1
+    enable: true
+    commit: true
 '''
 
 RETURN = '''
@@ -107,38 +95,20 @@ RETURN = '''
 '''
 
 from ansible.module_utils.basic import AnsibleModule
-from ansible.module_utils.basic import get_exception
+from ansible.module_utils.network.panos.panos import get_connection
+
 
 try:
-    from pan.xapi import PanXapiError
-    import pandevice
-    from pandevice import base
-    from pandevice import panorama
     from pandevice.errors import PanDeviceError
-    from pandevice import network
-
-    HAS_LIB = True
+    from pandevice.network import VirtualRouter
+    from pandevice.network import Bgp
+    from pandevice.network import BgpDampeningProfile
 except ImportError:
-    HAS_LIB = False
+    pass
 
 
 def setup_args():
     return dict(
-        ip_address=dict(
-            required=True,
-            help='IP address (or hostname) of PAN-OS device being configured'),
-        password=dict(
-            no_log=True,
-            help='Password credentials to use for auth unless I(api_key) is set'),
-        username=dict(
-            default='admin',
-            help='Username credentials to use for auth unless I(api_key) is set'),
-        api_key=dict(
-            no_log=True,
-            help='API key that can be used instead of I(username)/I(password) credentials'),
-        state=dict(
-            default='present', choices=['present', 'absent'],
-            help='Add or remove BGP Dampening Profile'),
         commit=dict(
             type='bool', default=True,
             help='Commit configuration if changed'),
@@ -172,70 +142,55 @@ def setup_args():
 
 
 def main():
-    argument_spec = setup_args()
+    helper = get_connection(
+        template=True,
+        template_stack=True,
+        with_state=True,
+        with_classic_provider_spec=True,
+        argument_spec=setup_args(),
+    )
 
-    module = AnsibleModule(argument_spec=argument_spec, supports_check_mode=False,
-                           required_one_of=[['api_key', 'password']])
-    if not HAS_LIB:
-        module.fail_json(msg='Missing required libraries.')
+    module = AnsibleModule(
+        argument_spec=helper.argument_spec,
+        supports_check_mode=True,
+        required_one_of=helper.required_one_of,
+    )
 
-    # Get the firewall / panorama auth.
-    auth = [module.params[x] for x in
-            ('ip_address', 'username', 'password', 'api_key')]
+    # Verify libs, initialize pandevice object tree.
+    parent = helper.get_pandevice_parent(module)
 
-    # exclude the default items from kwargs passed to the object
-    exclude_list = ['ip_address', 'username', 'password', 'api_key', 'state', 'commit']
-    # exclude these items from the kwargs passed to the object
-    exclude_list += ['vr_name']
-
-    # generate the kwargs for the object
-    obj_spec = dict((k, module.params[k]) for k in argument_spec.keys() if k not in exclude_list)
-
-    name = module.params['name']
-    state = module.params['state']
-    vr_name = module.params['vr_name']
-    commit = module.params['commit']
-
-    # create the new state object
-    new_obj = network.BgpDampeningProfile(**obj_spec)
-
-    changed = False
+    vr = VirtualRouter(module.params['vr_name'])
+    parent.add(vr)
     try:
-        # Create the device with the appropriate pandevice type
-        device = base.PanDevice.create_from_device(*auth)
-        network.VirtualRouter.refreshall(device)
+        vr.refresh()
+    except PanDeviceError as e:
+        module.fail_json(msg='Failed refresh: {0}'.format(e))
 
-        # grab the virtual router
-        vr = device.find(vr_name, network.VirtualRouter)
-        if vr is None:
-            raise ValueError('Virtual router {0} does not exist'.format(vr_name))
+    bgp = vr.find('', Bgp)
+    if bgp is None:
+        module.fail_json(msg='BGP is not configured for virtual router "{0}"'.format(vr.name))
 
-        # fetch the current settings
-        bgp = vr.find('', network.Bgp) or network.Bgp()
-        cur_obj = vr.find(name, network.BgpDampeningProfile, recursive=True)
+    listing = bgp.findall(BgpDampeningProfile)
+    spec = {
+        'name': module.params['name'],
+        'enable': module.params['enable'],
+        'cutoff': module.params['cutoff'],
+        'reuse': module.params['reuse'],
+        'max_hold_time': module.params['max_hold_time'],
+        'decay_half_life_reachable': module.params['decay_half_life_reachable'],
+        'decay_half_life_unreachable': module.params['decay_half_life_unreachable'],
+    }
+    obj = BgpDampeningProfile(**spec)
+    bgp.add(obj)
 
-        if state == 'present':
-            if cur_obj is None or not new_obj.equal(cur_obj, compare_children=False):
-                bgp.add(new_obj)
-                new_obj.apply()
-                changed = True
-        elif state == 'absent':
-            if cur_obj is not None:
-                cur_obj.delete()
-                changed = True
-        else:
-            module.fail_json(msg='[%s] state is not implemented yet' % state)
-    except (PanDeviceError, KeyError):
-        exc = get_exception()
-        module.fail_json(msg=exc.message)
+    # Apply the requested state.
+    changed = helper.apply_state(obj, listing, module)
 
-    if commit and changed:
-        device.commit(sync=True, exception=True)
+    # Optional commit.
+    if changed and module.params['commit']:
+        helper.commit(module)
 
-    if changed:
-        module.exit_json(msg='BGP dampening profile update successful.', changed=changed)
-    else:
-        module.exit_json(msg='no changes required.', changed=changed)
+    module.exit_json(changed=changed, msg='done')
 
 
 if __name__ == '__main__':
