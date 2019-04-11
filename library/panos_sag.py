68b5ffa1b7f88a0da6f4b6c2d7ddb43b08c4b78e
Garfield Freeman <gfreeman@paloaltonetworks.com>
2019-04-10 23:24:42 -0700

    Updating panos_sag

--- library/panos_sag.py
+++ library/panos_sag.py
@@ -22,6 +22,10 @@ description:
     - Create a static address group object in the firewall used for policy rules.
 author: "Vinay Venkataraghavan @vinayvenkat"
 version_added: "2.4"
+deprecated:
+    alternative: Use M(panos_address_group) instead.
+    removed_in: '2.12'
+    why: This module's functionality is a subset of M(panos_address_group).
 requirements:
     - pan-python can be obtained from PyPI U(https://pypi.python.org/pypi/pan-python)
     - pandevice can be obtained from PyPI U(https://pypi.python.org/pypi/pandevice)
@@ -99,7 +103,7 @@ RETURN = '''
 '''
 
 ANSIBLE_METADATA = {'metadata_version': '1.1',
-                    'status': ['preview'],
+                    'status': ['deprecated'],
                     'supported_by': 'community'}
 
 from ansible.module_utils.basic import AnsibleModule, get_exception
@@ -245,6 +249,9 @@ def main():
     )
     module = AnsibleModule(argument_spec=argument_spec, supports_check_mode=False,
                            required_one_of=[['api_key', 'password']])
+
+    module.deprecate('This module has been deprecated; use panos_address_group', '2.12')
+
     if not HAS_LIB:
         module.fail_json(msg='pan-python is required for this module')
 
