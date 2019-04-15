1b30b7e110fb634c045efe066504b3e3c2c5b63d
Garfield Freeman <gfreeman@paloaltonetworks.com>
2019-04-15 08:14:47 -0700

    Updating panos_bgp_conditional_advertisement

--- library/panos_bgp_conditional_advertisement.py
+++ library/panos_bgp_conditional_advertisement.py
@@ -25,132 +25,97 @@ ANSIBLE_METADATA = {'metadata_version': '1.1',
 DOCUMENTATION = '''
 ---
 module: panos_bgp_conditional_advertisement
-short_description: Configures a BGP Authentication Profile
+short_description: Configures a BGP conditional advertisement.
 description:
     - Use BGP to publish and consume routes from disparate networks.
+    - In the PAN-OS GUI, this resource cannot be created without also creating at least
+      one non-exist filter and one advertise filter. The API behaves a little differently:
+      you can create the conditional advertisement itself, but the API will start throwing
+      errors if you try to update it and there is not at least one non-exist filter and
+      one advertise filter.
+    - In order for a conditional advertisement to be valid, you must specify at least one
+      non-exist and one advertise filter.
+    - When modifying a BGP conditional advertisement, any filters attached are left as-is,
+      unless I(advertise_filter) or I(non_exist_filter) are specified.
 author: "Joshua Colson (@freakinhippie)"
-version_added: "2.9"
+version_added: "2.8"
 requirements:
     - pan-python can be obtained from PyPI U(https://pypi.python.org/pypi/pan-python)
     - pandevice can be obtained from PyPI U(https://pypi.python.org/pypi/pandevice)
 notes:
-    - Checkmode is not supported.
-    - Panorama is NOT supported.
+    - Checkmode is supported.
+    - Panorama is supported.
+extends_documentation_fragment:
+    - panos.transitional_provider
+    - panos.full_template_support
+    - panos.state
 options:
-    ip_address:
-        description:
-            - IP address (or hostname) of PAN-OS device being configured.
-            required: True
-    username:
-        description:
-            - Username credentials to use for auth unless I(api_key) is set.
-            default: admin
-    password:
-        description:
-            - Password credentials to use for auth unless I(api_key) is set.
-    api_key:
-        description:
-            - API key that can be used instead of I(username)/I(password) credentials.
-    state:
-        description:
-            - Add or remove BGP Conditional Advertisement Policy.
-                - present
-                - absent
-            default: present
     commit:
         description:
             - Commit configuration if changed.
-            default: True
+        default: True
+        type: bool
     vr_name:
         description:
-            - Name of the virtual router; it must already exist; see panos_virtual_router.
-            default: default
-    advertisement_filter:
+            - Name of the virtual router; it must already exist and have BGP configured.
+            - See M(panos_virtual_router).
+        default: default
+    advertise_filter:
         description:
-            - Advertisement filter object returned by panos_bgp_policy_filter; only needed on creation.
+            - B(Deprecated)
+            - Use M(panos_bgp_policy_filter) to define filters after creation.
+            - HORIZONTALLINE
+            - Advertisement filter object returned by M(panos_bgp_policy_filter).
     non_exist_filter:
         description:
-            - Non-Exist filter object returned by panos_bgp_policy_filter; only needed on creation.
+            - B(Deprecated)
+            - Use M(panos_bgp_policy_filter) to define filters after creation.
+            - HORIZONTALLINE
+            - Non-Exist filter object returned by M(panos_bgp_policy_filter).
     enable:
         description:
             - Enable this policy.
+        type: bool
     name:
         description:
             - Name of Conditional Advertisement policy.
-            required: True
+        required: True
     used_by:
         description:
             - List of Peer Groups using this policy.
+        type: list
 '''
 
 EXAMPLES = '''
-    - name: Create BGP Conditional Advertisement Rule
-      panos_bgp_conditional_advertisement:
-        ip_address: '{{ ip_address }}'
-        username: '{{ username }}'
-        password: '{{ password }}'
-        vr_name: default
-        name: cond-rule-01
-        enable: true
-        non_exist_filter: '{{ non_exist.panos_obj }}'
-        advertise_filter: '{{ advertise.panos_obj }}'
-
-    - name: Update BGP Conditional Advertisement Rule
-      panos_bgp_conditional_advertisement:
-        ip_address: '{{ ip_address }}'
-        username: '{{ username }}'
-        password: '{{ password }}'
-        vr_name: default
-        name: cond-rule-01
-        enable: false
-
-    - name: Remove BGP Conditional Advertisement Rule
-      panos_bgp_conditional_advertisement:
-        ip_address: '{{ ip_address }}'
-        username: '{{ username }}'
-        password: '{{ password }}'
-        state: absent
-        vr_name: default
-        name: cond-rule-01
+- name: Create BGP Conditional Advertisement Rule
+  panos_bgp_conditional_advertisement:
+    provider: '{{ provider }}'
+    name: 'cond-rule-01'
+    enable: true
+    non_exist_filter: '{{ non_exist.panos_obj }}'
+    advertise_filter: '{{ advertise.panos_obj }}'
 '''
 
 RETURN = '''
 # Default return values
 '''
 
+
 from ansible.module_utils.basic import AnsibleModule
-from ansible.module_utils.basic import get_exception
+from ansible.module_utils.network.panos.panos import get_connection
+
 
 try:
-    from pan.xapi import PanXapiError
-    import pandevice
-    from pandevice import base
-    from pandevice import panorama
     from pandevice.errors import PanDeviceError
-    from pandevice import network
-
-    HAS_LIB = True
+    from pandevice.network import VirtualRouter
+    from pandevice.network import Bgp
+    from pandevice.network import BgpPolicyConditionalAdvertisement
 except ImportError:
-    HAS_LIB = False
+    pass
 
 
 def setup_args():
     return dict(
-        ip_address=dict(
-            required=True,
-            help='IP address (or hostname) of PAN-OS device being configured'),
-        password=dict(
-            no_log=True,
-            help='Password credentials to use for auth unless I(api_key) is set'),
-        username=dict(
-            default='admin',
-            help='Username credentials to use for auth unless I(api_key) is set'),
-        api_key=dict(
-            no_log=True,
-            help='API key that can be used instead of I(username)/I(password) credentials'),
-        state=dict(
-            default='present', choices=['present', 'absent'],
-            help='Add or remove BGP Conditional Advertisement Policy'),
         commit=dict(
             type='bool', default=True,
             help='Commit configuration if changed'),
@@ -178,92 +143,58 @@ def setup_args():
 
 
 def main():
-    argument_spec = setup_args()
+    helper = get_connection(
+        template=True,
+        template_stack=True,
+        with_state=True,
+        with_classic_provider_spec=True,
+        argument_spec=setup_args(),
+    )
 
-    module = AnsibleModule(argument_spec=argument_spec, supports_check_mode=False,
-                           required_one_of=[['api_key', 'password']])
-    if not HAS_LIB:
-        module.fail_json(msg='Missing required libraries.')
+    module = AnsibleModule(
+        argument_spec=helper.argument_spec,
+        supports_check_mode=True,
+        required_one_of=helper.required_one_of,
+    )
 
-    # Get the firewall / panorama auth.
-    auth = [module.params[x] for x in
-            ('ip_address', 'username', 'password', 'api_key')]
+    parent = helper.get_pandevice_parent(module)
 
-    # exclude the default items from kwargs passed to the object
-    exclude_list = ['ip_address', 'username', 'password', 'api_key', 'state', 'commit']
-    # exclude these items from the kwargs passed to the object
-    exclude_list += ['vr_name', 'non_exist_filter', 'advertise_filter']
-
-    # generate the kwargs for the object
-    obj_spec = dict((k, module.params[k]) for k in argument_spec.keys() if k not in exclude_list)
-
-    name = module.params['name']
-    state = module.params['state']
-    vr_name = module.params['vr_name']
-    commit = module.params['commit']
-    ne_filter = module.params['non_exist_filter']
-    ad_filter = module.params['advertise_filter']
-
-    # create the new state object
-    new_obj = network.BgpPolicyConditionalAdvertisement(**obj_spec)
-
-    changed = False
+    vr = VirtualRouter(module.params['vr_name'])
+    parent.add(vr)
     try:
-        # Create the device with the appropriate pandevice type
-        device = base.PanDevice.create_from_device(*auth)
-        network.VirtualRouter.refreshall(device)
+        vr.refresh()
+    except PanDeviceError as e:
+        module.fail_json(msg='Failed refresh: {0}'.format(e))
 
-        # grab the virtual router
-        vr = device.find(vr_name, network.VirtualRouter)
-        if vr is None:
-            raise ValueError('Virtual router {0} does not exist'.format(vr_name))
+    bgp = vr.find('', Bgp)
+    if bgp is None:
+        module.fail_json(msg='BGP is not configured on virtual router {0}'.format(vr.name))
 
-        # fetch the current settings
-        bgp = vr.find('', network.Bgp) or network.Bgp()
-        cur_obj = vr.find(name, network.BgpPolicyConditionalAdvertisement, recursive=True)
+    listing = bgp.findall(BgpPolicyConditionalAdvertisement)
 
-        if state == 'present':
-            if cur_obj is None or not new_obj.equal(cur_obj, compare_children=False):
-                if cur_obj is None and (ne_filter is None or ad_filter is None):
-                    raise ValueError(
-                        ' '.join(
-                            [
-                                'Creating a new Conditional Advertisement policy requires:',
-                                'non_exist_filter and advertise_filter'
-                            ]
-                        )
-                    )
-                elif cur_obj is None:
-                    import pickle
-                    from base64 import b64decode
-                    # deserialize BgpPolicyNonExistFilter object
-                    ne_filter_obj = pickle.loads(b64decode(ne_filter))
-                    # deserialize BgpPolicyAdvertisementFilter object
-                    ad_filter_obj = pickle.loads(b64decode(ad_filter))
-                    # add them both to the new policy
-                    new_obj.add(ne_filter_obj)
-                    new_obj.add(ad_filter_obj)
-                bgp.add(new_obj)
-                # use create() here instead of apply because the children are required
-                new_obj.create()
-                changed = True
-        elif state == 'absent':
-            if cur_obj is not None:
-                cur_obj.delete()
-                changed = True
-        else:
-            module.fail_json(msg='[%s] state is not implemented yet' % state)
-    except (PanDeviceError, KeyError):
-        exc = get_exception()
-        module.fail_json(msg=exc.message)
+    spec = {
+        'name': module.params['name'],
+        'enable': module.params['enable'],
+        'used_by': module.params['used_by'],
+    }
+    obj = BgpPolicyConditionalAdvertisement(**spec)
+    bgp.add(obj)
 
-    if commit and changed:
-        device.commit(sync=True, exception=True)
+    # TODO(gfreeman) - Remove this in 2.12.
+    for ansible_param in ('non_exist_filter', 'advertise_filter'):
+        val = module.params[ansible_param]
+        if val is not None:
+            import pickle
+            from base64 import b64decode
+            module.deprecate('Param {0} is deprecated'.format(ansible_param), '2.12')
+            filter_obj = pickle.loads(b64decode(val))
+            obj.add(filter_obj)
 
-    if changed:
-        module.exit_json(msg='BGP conditional advertisement policy update successful.', changed=changed)
-    else:
-        module.exit_json(msg='no changes required.', changed=changed)
+    changed = helper.apply_state(obj, listing, module)
+    if changed and module.params['commit']:
+        helper.commit(module)
+
+    module.exit_json(changed=changed, msg='done')
 
 
 if __name__ == '__main__':
