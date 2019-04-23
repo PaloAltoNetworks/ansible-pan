a71c1d0a3630f7c5840490fdf8a7adf61aec9217
Garfield Freeman <gfreeman@paloaltonetworks.com>
2019-04-23 14:41:55 -0700

    disallow "shared" for security rules

--- library/panos_security_rule.py
+++ library/panos_security_rule.py
@@ -331,6 +331,7 @@ def main():
         rulebase=True,
         with_state=True,
         with_classic_provider_spec=True,
+        error_on_shared=True,
         argument_spec=dict(
             rule_name=dict(required=True),
             source_zone=dict(type='list', default=['any']),
