93baf6895fdce5caad6cba01fe37a68e2766f103
Garfield Freeman <gfreeman@paloaltonetworks.com>
2020-01-17 10:58:46 -0800

    updating panos_object_facts for flexible search

--- library/panos_object_facts.py
+++ library/panos_object_facts.py
@@ -28,7 +28,9 @@ module: panos_object_facts
 short_description: Retrieve facts about objects on PAN-OS devices.
 description:
     - Retrieves tag information objects on PAN-OS devices.
-author: "Michael Richardson (@mrichardson03)"
+author:
+    - Michael Richardson (@mrichardson03)
+    - Garfield Lee Freeman (@shinmog)
 version_added: "2.8"
 requirements:
     - pan-python can be obtained from PyPI U(https://pypi.python.org/pypi/pan-python)
@@ -42,19 +44,25 @@ options:
     name:
         description:
             - Name of object to retrieve.
-            - Mutually exclusive with I(name_regex).
+            - Mutually exclusive with I(name_regex) and I(field).
     name_regex:
         description:
             - A python regex for an object's name to retrieve.
-            - Mutually exclusive with I(name).
-    value:
+            - Mutually exclusive with I(name) and I(field).
+    field:
         description:
-            - IP of object to retrieve.
-            - Mutually exclusive with I(value_regex).
-    value_regex:
+            - The field to search instead of name.
+            - Mutually exclusive with I(name) and I(name_regex)
+    field_search_type:
         description:
-            - A python regex for an object's IP to retrieve.
-            - Mutually exclusive with I(value).
+            - The type of search to perform when doing a I(field) search.
+        choices:
+            - exact
+            - regex
+        default: 'exact'
+    field_search_value:
+        description:
+            - The value for the I(field_search) and I(field) specified.
     object_type:
         description:
             - Type of object to retrieve.
@@ -138,8 +146,34 @@ def colorize(obj, object_type):
     return ans
 
 
+def matches(obj, field, exact=None, regex=None):
+    is_str = True
+    about = obj.about(field)['About']
+    if isinstance(about, dict):
+        is_str = about.get('Type', 'string') == 'string'
+
+    if exact is not None:
+        if is_str:
+            return getattr(obj, field) == exact
+        else:
+            for x in getattr(obj, field, []):
+                if x == exact:
+                    return True
+            return False
+    elif regex is not None:
+        if is_str:
+            return regex.search(getattr(obj, field)) is not None
+        else:
+            for x in getattr(obj, field, []):
+                if regex.search(x):
+                    return True
+            return False
+
+    return False
+
+
 def main():
-    name_params = ['name', 'name_regex', 'value', 'value_regex']
+    name_params = ['name', 'name_regex', 'field']
     obj_types = {
         'address': objects.AddressObject,
         'address-group': objects.AddressGroup,
@@ -155,8 +189,9 @@ def main():
         argument_spec=dict(
             name=dict(),
             name_regex=dict(),
-            value=dict(),
-            value_regex=dict(),
+            field=dict(),
+            field_search_type=dict(choices=['exact', 'regex'], default='exact'),
+            field_search_value=dict(),
             object_type=dict(default='address', choices=obj_types.keys()),
         ),
     )
@@ -185,32 +220,42 @@ def main():
         if obj:
             results = colorize(obj, object_type)
             ans_objects.append(results)
-    elif module.params['value'] is not None:
-        for x in obj_listing:
-            AdressObject = x.about()
-            if AdressObject['value'] == module.params['value']:
-                ans_objects.append(colorize(x, object_type))
-    elif module.params['value_regex'] is not None:
-        try:
-            matcher = re.compile(module.params['value_regex'])
-        except Exception as e:
-            module.fail_json(msg='Invalid regex: {0}'.format(e))
-
-        for x in obj_listing:
-            AdressObject = x.about()
-            if matcher.search(AdressObject['value']) is not None:
-                ans_objects.append(colorize(x, object_type))
-    else:
+    elif module.params['name_regex']:
         try:
             matcher = re.compile(module.params['name_regex'])
         except Exception as e:
             module.fail_json(msg='Invalid regex: {0}'.format(e))
 
-        for x in obj_listing:
-            if matcher.search(x.uid) is not None:
-                ans_objects.append(colorize(x, object_type))
+        ans_objects = [
+            colorize(x, object_type)
+            for x in obj_listing
+            if matcher.search(x.uid) is not None
+        ]
+    else:
+        if not hasattr(obj_type(), module.params['field']):
+            module.fail_json(msg='Object({0}) does not have field({1})'.format(object_type, module.params['field']))
+        elif not module.params['field_search_value']:
+            module.fail_json(msg='Searching a field requires that field_search_value is specified')
 
-    module.exit_json(changed=False, results=results, objects=ans_objects)
+        if module.params['field_search_type'] == 'exact':
+            ans_objects = [
+                colorize(x, object_type)
+                for x in obj_listing
+                if matches(x, module.params['field'], exact=module.params['field_search_value'])
+            ]
+        elif module.params['field_search_type'] == 'regex':
+            try:
+                regex = re.compile(module.params['field_search_value'])
+            except Exception as e:
+                module.fail_json(msg='Invalid field regex: {0}'.format(e))
+
+            ans_objects = [
+                colorize(x, object_type)
+                for x in obj_listing
+                if matches(x, module.params['field'], regex=regex)
+            ]
+
+    module.exit_json(changed=False, ansible_module_results=results, objects=ans_objects)
 
 
 if __name__ == '__main__':
